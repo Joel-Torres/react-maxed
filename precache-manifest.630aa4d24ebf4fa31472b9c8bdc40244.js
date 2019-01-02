bb572ef07491eb80a0bacf9b741369649b8392b5
Joel Torres <joel.torres.dev@gmail.com>
2019-01-01 18:25:50 -0800

    Updates

---
+++ precache-manifest.630aa4d24ebf4fa31472b9c8bdc40244.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "df6ca968ea7974bb4491",
+    "url": "/react-maxed/static/js/runtime~main.df6ca968.js"
+  },
+  {
+    "revision": "896d6d28e0fc9dcca3d7",
+    "url": "/react-maxed/static/js/main.896d6d28.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/react-maxed/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "896d6d28e0fc9dcca3d7",
+    "url": "/react-maxed/static/css/main.38dc68b1.chunk.css"
+  },
+  {
+    "revision": "01d874dc4a2dd40ede0b33ed09b83855",
+    "url": "/react-maxed/index.html"
+  }
+];
